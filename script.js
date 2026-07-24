e42090ca18bd1919659e6bfb2d053e9a7f46a00c
Arthur Zin <zarthy10111@gmail.com>
2026-07-24 17:14:04 -0400

    modified the app1 in order to make an app2 (its horribly optimized.) Its a cheese clicker that uses the interface from app1 for upgrades.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 // Make the DIV element draggable:
 dragElement(document.getElementById("cheeseDraggable"));
 dragElement(document.querySelector("#app"))
+dragElement(document.querySelector("#app2"))
 dragElement(document.querySelector("#loveCheese"))
 dragElement(document.querySelector("#welcome"))
 
@@ -72,6 +73,11 @@ var appScreenClose = document.querySelector("#appclose")
 var appScreenOpen = document.querySelector("#appopen")
 var appScreen = document.querySelector("#app")
 
+
+var app2ScreenClose = document.querySelector("#app2close")
+var app2ScreenOpen = document.querySelector("#app2open")
+var app2Screen = document.querySelector("#app2")
+
 var biggestIndex = 1;
 
 var topBar = document.querySelector("#top")
@@ -110,6 +116,16 @@ appScreenClose.addEventListener("click", function() {
 appScreenOpen.addEventListener("click", function() {
   openWindow(appScreen)
   handleIconTap(appScreen);
+})
+
+app2ScreenClose.addEventListener("click", function() {
+  closeWindow(app2Screen)
+  closeWindow(document.querySelector("#app2"));
+})
+app2ScreenOpen.addEventListener("click", function() {
+  openWindow(app2Screen)
+  openWindow(appScreen)
+  handleIconTap2(app2Screen);
 })
 
 welcomeScreenOpen.addEventListener("click", function() {
@@ -164,8 +180,8 @@ function handleIconTap(element){
       rating: "SR",
       content: ` 
     
-      <h3 style="margin: 0 0 4px 0; font-family: 'Times New Roman', Times, serif;">The Chosen Cheese</h3>
-      <p style="margin: 0 0 12px 0; font-size: 12px; font-weight: 700; letter-spacing: 1px; color: #3f8b75;">SSR</p>
+      <h3 style="margin: 0 0 4px 0; font-family: 'Times New Roman', Times, serif;">Ice Cream</h3>
+      <p style="margin: 0 0 12px 0; font-size: 12px; font-weight: 700; letter-spacing: 1px; color: #3f8b75;">SR</p>
       <p>
         I love this relic--but not as much as The Chosen Cheese. It's pretty up there, though.
         Ice Cream is just awesome!
@@ -177,8 +193,8 @@ function handleIconTap(element){
       title: "Psalm Cylinder",
       rating: "R",
       content: ` 
-        <h3 style="margin: 0 0 4px 0; font-family: 'Times New Roman', Times, serif;">The Chosen Cheese</h3>
-      <p style="margin: 0 0 12px 0; font-size: 12px; font-weight: 700; letter-spacing: 1px; color: #3f8b75;">SSR</p>
+        <h3 style="margin: 0 0 4px 0; font-family: 'Times New Roman', Times, serif;">Psalm Cylinder</h3>
+      <p style="margin: 0 0 12px 0; font-size: 12px; font-weight: 700; letter-spacing: 1px; color: #3f8b75;">R</p>
       <p>
         These are cute little tracks, but they aren't particularly crazy. They're guarranteed, which is why they aren't SR.
         Still, they are fun and the Whispering Vaults is rather cool.
@@ -192,8 +208,8 @@ function handleIconTap(element){
       rating: "C",
       content: ` 
 
-      <h3 style="margin: 0 0 4px 0; font-family: 'Times New Roman', Times, serif;">The Chosen Cheese</h3>
-      <p style="margin: 0 0 12px 0; font-size: 12px; font-weight: 700; letter-spacing: 1px; color: #3f8b75;">SSR</p>
+      <h3 style="margin: 0 0 4px 0; font-family: 'Times New Roman', Times, serif;">Dart</h3>
+      <p style="margin: 0 0 12px 0; font-size: 12px; font-weight: 700; letter-spacing: 1px; color: #3f8b75;">C</p>
       <p>
              Noob item. Eberybody has this one.
       </p>
@@ -207,8 +223,8 @@ function handleIconTap(element){
       rating: "B",
       content: ` 
       <p>
-        <h3 style="margin: 0 0 4px 0; font-family: 'Times New Roman', Times, serif;">The Chosen Cheese</h3>
-      <p style="margin: 0 0 12px 0; font-size: 12px; font-weight: 700; letter-spacing: 1px; color: #3f8b75;">SSR</p>
+        <h3 style="margin: 0 0 4px 0; font-family: 'Times New Roman', Times, serif;">Gatchapon</h3>
+      <p style="margin: 0 0 12px 0; font-size: 12px; font-weight: 700; letter-spacing: 1px; color: #3f8b75;">B</p>
       <p>
              Cool item. gamba. 
       </p>
@@ -222,8 +238,8 @@ function handleIconTap(element){
       rating: "A",
       content: ` 
       <p>
-        <h3 style="margin: 0 0 4px 0; font-family: 'Times New Roman', Times, serif;">The Chosen Cheese</h3>
-      <p style="margin: 0 0 12px 0; font-size: 12px; font-weight: 700; letter-spacing: 1px; color: #3f8b75;">SSR</p>
+        <h3 style="margin: 0 0 4px 0; font-family: 'Times New Roman', Times, serif;">AA Turret</h3>
+      <p style="margin: 0 0 12px 0; font-size: 12px; font-weight: 700; letter-spacing: 1px; color: #3f8b75;">A</p>
       <p>
           Very good utility throughout the entire game. Pesky air crafts.
       </p>
@@ -262,6 +278,178 @@ function addToSideBar(index) {
     sidebar.appendChild(newDiv);
   }
 
+  document.querySelector("#sidebar").innerHTML = "";
+
+  for (let i = 0; i < content.length; i++) {
+    addToSideBar(i);
+  }
+
+  setContent(0);
+}
+
+// app2  stuff (im lowk lazy so im just repurposing things)
+var totalCheese = 0;
+var cheesePerSecond = 1;
+
+function updateCheese(){
+  totalCheese += cheesePerSecond;
+  var counter = document.querySelector("#cheeseCounter");
+  if(counter){
+    counter.innerHTML = "Cheese: " + totalCheese;
+  }
+}
+setInterval(updateCheese, 100)
+
+function handleIconTap2(element){
+  if(element.classList.contains("selected")){
+    deselectIcon(element) 
+    openWindow(window)
+  }else{
+    selectIcon(element)
+  }
+
+  // content stuff...
+
+  var content = [  
+    {
+      title: "Chosen Cheese Upgrade",
+      boost: "+99999 cheese",
+      cost: "99999 cheese",
+      content: ` 
+      <p>
+        <h3 style="margin: 0 0 4px 0; font-family: 'Times New Roman', Times, serif;">The Chosen Cheese ($99999)</h3>
+      <p style="margin: 0 0 12px 0; font-size: 12px; font-weight: 700; letter-spacing: 1px; color: #3f8b75;">+99999 cheese</p>
+      <p>
+          Upgrade Chosen Cheese Upgrade. 
+      </p>
+      </p>
+      `
+    },
+
+    {
+      title: "Ice Cream Upgrade",
+      boost: "+6700 cheese",
+      cost: "16767 cheese",
+      content: ` 
+      <p>
+        <h3 style="margin: 0 0 4px 0; font-family: 'Times New Roman', Times, serif;">Ice Cream Upgrade ($16767)</h3>
+      <p style="margin: 0 0 12px 0; font-size: 12px; font-weight: 700; letter-spacing: 1px; color: #3f8b75;">+6700 cheese</p>
+      <p>
+          Upgrade Ice Cream Upgrade. 
+      </p>
+      </p>
+      `
+    },
+
+    {
+      title: "Psalm Cylinder Upgrade",
+      boost: "+1500 cheese",
+      cost: "3750 cheese",
+      content: ` 
+      <p>
+        <h3 style="margin: 0 0 4px 0; font-family: 'Times New Roman', Times, serif;">Psalm Cylinder ($3750)</h3>
+      <p style="margin: 0 0 12px 0; font-size: 12px; font-weight: 700; letter-spacing: 1px; color: #3f8b75;">+1500 cheese</p>
+      <p>
+          Upgrade Psalm Cylinder Upgrade. 
+      </p>
+      </p>
+      `
+    },
+
+    {
+      title: "AA Turret Upgrade",
+      boost: "+550 cheese",
+      cost: "1200 cheese",
+      content: ` 
+      <p>
+        <h3 style="margin: 0 0 4px 0; font-family: 'Times New Roman', Times, serif;">AA Turret Upgrade($1200)</h3>
+      <p style="margin: 0 0 12px 0; font-size: 12px; font-weight: 700; letter-spacing: 1px; color: #3f8b75;">+550 cheese</p>
+      <p>
+          Upgrade AA Turret Upgrade.
+      </p>
+      </p>
+      `
+    },
+     {
+      title: "Gatchapon Upgrade",
+      boost: "+150 cheese",
+      cost: "250 cheese",
+      content: ` 
+      <p>
+        <h3 style="margin: 0 0 4px 0; font-family: 'Times New Roman', Times, serif;">Gatchapon Upgrade($250)</h3>
+      <p style="margin: 0 0 12px 0; font-size: 12px; font-weight: 700; letter-spacing: 1px; color: #3f8b75;">+150 cheese</p>
+      <p>
+          Upgrade Gatchapon Upgrade.
+      </p>
+      </p>
+      `
+    },
+     {
+      title: "Dart Upgrade",
+      boost: "+15 cheese",
+      cost: "35 cheese",
+      content: ` 
+      <p>
+        <h3 style="margin: 0 0 4px 0; font-family: 'Times New Roman', Times, serif;">AA Turret Upgrade($35)</h3>
+      <p style="margin: 0 0 12px 0; font-size: 12px; font-weight: 700; letter-spacing: 1px; color: #3f8b75;">+15 cheese</p>
+      <p>
+          Upgrade AA Turret Upgrade.
+      </p>
+      </p>
+      `
+    },
+  ]
+
+  ///
+
+function setContent(index) {
+  var contentArea = document.querySelector("#contentArea");
+  contentArea.innerHTML = content[index].content;
+  var costText = content[index].cost;
+  var cost = parseInt(costText.replace("cheese", "").trim());
+  if(totalCheese >= cost){
+    totalCheese -=cost
+    upgradeCheese(index);
+  }
+}
+
+function upgradeCheese(index){
+  var boost = content[index].boost;
+  var upgrade = parseInt(boost.replace("cheese", "").trim());
+  cheesePerSecond += upgrade;
+  var CPScounter = document.querySelector("#cheesePerSecond");
+  if(CPScounter){
+    CPScounter.innerHTML = "CPS: "+ cheesePerSecond;
+  }
+}
+
+function addToSideBar(index) {
+    var sidebar = document.querySelector("#sidebar");
+    var entry = content[index];
+    var newDiv = document.createElement("div");
+    newDiv.style = "cursor: pointer; margin-bottom: 12px;";
+
+      newDiv.innerHTML = `
+      <p style="margin: 0px; font-weight: 700;">
+        ${entry.title}
+      </p>
+      <p style="font-size: 12px; margin: 0px;">
+        ${entry.boost}
+      </p>
+
+      <p style="font-size: 12px; margin: 0px;">
+        cost: ${entry.cost}
+      </p>
+    `;
+
+    newDiv.addEventListener("click", function() {
+      setContent(index);
+    });
+
+    sidebar.appendChild(newDiv);
+  }
+
+  document.querySelector("#sidebar").innerHTML = "";
   for (let i = 0; i < content.length; i++) {
     addToSideBar(i);
   }
